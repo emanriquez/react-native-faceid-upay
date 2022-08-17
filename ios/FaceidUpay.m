4e3a2fecaa8dd20fa3d7e8c3e5bb496d2d9dc4fb
Elias Manriquez <eliasmanriquez@UPAY-MAC-49.local>
2022-08-17 19:56:22 -0400

    add ios

--- ios/FaceidUpay.m
+++ ios/FaceidUpay.m
@@ -11,12 +11,10 @@
     return dispatch_get_main_queue();
 }
 
-
 RCTResponseSenderBlock requestPermissionsResolve;
 bool hasPendingOperation;
 
 RCT_EXPORT_MODULE()
-
 
 
 RCT_EXPORT_METHOD(docLivenessFlow
@@ -25,50 +23,31 @@ RCT_EXPORT_METHOD(docLivenessFlow
                   :(RCTResponseSenderBlock)callback)
 {
   RCTLogInfo(@"INIT NATIVE FACEID@");
-    NSLog(@"INIT CAPTURE AND LIVENESS");
     requestPermissionsResolve = callback;
     NSData *datos = [Data dataUsingEncoding:NSUTF8StringEncoding];
-    //NSArray jsonOutput = [NSJSONSerialization JSONObjectWithData:datos options:0 error:nil];
     NSError *e = nil;
     NSDictionary *jsonOutput = [NSJSONSerialization JSONObjectWithData: datos options: NSJSONReadingMutableContainers error: &e];
-
-    NSLog(@"%@",jsonOutput);
-    NSLog(@"%@",[jsonOutput objectForKey:@"BaseURL"]);
-
     @try {
-
-        NSString *bioEndpoint = [jsonOutput objectForKey:@"BaseURL"];
-        NSString *deviceKey = [jsonOutput objectForKey:@"deviceKeyIdentifier"];
-        NSString *projectSecret = [jsonOutput objectForKey:@"projectSecret"];
-        NSString *keyText = [jsonOutput objectForKey:@"licenceKey"];
-        NSString *docLicense = [jsonOutput objectForKey:@"dLicense"];
-        NSString *faceReportHost = [jsonOutput objectForKey:@"ReportHost"];
-
-        [BioCaller docLivenessFlowWithAppDelegate:[[UIApplication sharedApplication] delegate] dLicense:docLicense projectSecret:projectSecret bioEndPoint:bioEndpoint bioKey:deviceKey keyText:keyText reportsEndpoint:faceReportHost];
-        //callback(@[[NSNull null], @"data"]);
+        NSString *apikey = [jsonOutput objectForKey:@"apikey"];
+        NSString *url = [jsonOutput objectForKey:@"url"];
+        bool *onboarding = [jsonOutput objectForKey:@"onboarding"];
+        bool *gov = [jsonOutput objectForKey:@"gov"];
+        [BioCaller CreateSessionWithAppDelegate:[[UIApplication sharedApplication] delegate] apiKey:apikey url:url tour:true function:"nul"];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(callBackApp) name:@"FIDNotification" object:nil];
         hasPendingOperation = YES;
     } @catch (NSException *exception) {
         callback(@[exception.reason, [NSNull null]]);
-        //callback(@[ @"TASK_DUMPED", @"{\"success\": false}" ]);
-        //reject(@"event_failure", @"no event id returned", nil);
-        //pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: exception.name];
-        //[self.commandDelegate sendPluginResult:pluginResult callbackId:commandAux.callbackId];
+        
     }
 }
 
 
-
 - (void)callBackApp{
-        if (BioCaller.getFaceIdResultData  != nil && hasPendingOperation == YES){
+        if (BioCaller.getReturnapp()  != nil){
             hasPendingOperation = NO;
             [[NSNotificationCenter defaultCenter] removeObserver:self];
-            NSLog(@"Got a notification");
-            NSLog(BioCaller.getFaceIdResultData);
-            requestPermissionsResolve(@[[NSNull null],BioCaller.getFaceIdResultData]);
-            //resolve(BioCaller.getFaceIdResultData);
-            //pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: BioCaller.getFaceIdResultData];
-            //[self.commandDelegate sendPluginResult:pluginResult callbackId:commandAux.callbackId];
+            requestPermissionsResolve(@[[NSNull null],BioCaller.getReturnapp()]);
+          
         }
 }
 
